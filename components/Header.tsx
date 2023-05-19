b6877ed396825862b95352147133ec58683be656
Roman Kohut <romankohut1996@gmail.com>
2023-05-19 12:26:59 +0300

    fix: route bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header: FC = () => {
   return (
     <div className='w-full bg-primary-100 h-16 flex items-center px-7 justify-between'>
       <h1 className='text-xl text-primary'>Projects</h1>
-      <Link href={'create_project'} className={'bg-primary p-2.5 rounded-xl'}>
+      <Link href={'/create_project'} className={'bg-primary p-2.5 rounded-xl'}>
         Create project
       </Link>
     </div>
